dd9033d54e7a61dd7f1bd00ecdcea237daaa0ed4
Nasty <anastasia.sklarow@stud.uni-due.de>
2018-05-17 12:52:46 +0200

    Versuche

--- app/src/main/java/com/mp3/analukpat/mp3_player/Titel.java
+++ app/src/main/java/com/mp3/analukpat/mp3_player/Titel.java
@@ -8,7 +8,7 @@ public class Titel extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_titel);
-        //sdf
+        setContentView(R.layout.activity_einzelwiedergabe);
+
     }
 }
