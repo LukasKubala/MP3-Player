662ba45286df5ae4fdf2167b0e750a02f9038cbb
Nasty <anastasia.sklarow@stud.uni-due.de>
2018-05-24 21:40:43 +0200

    Neue Activities wg Tabhost

--- app/src/main/java/com/mp3/analukpat/mp3_player/MainTabsActivity.java
+++ app/src/main/java/com/mp3/analukpat/mp3_player/MainTabsActivity.java
@@ -3,11 +3,11 @@ package com.mp3.analukpat.mp3_player;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AddToPlaylistActivity extends AppCompatActivity {
-    //Sicht für hinzufügen von Liedern in die Playlist
+public class MainTabsActivity extends AppCompatActivity {
+//Activity, die den Tabhost für Titel, Interpreten und Playlists aufruft
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_to_playlist);
+        setContentView(R.layout.activity_main_tabs);
     }
 }
--- app/src/main/java/com/mp3/analukpat/mp3_player/WiedergabeTabsActivity.java
+++ app/src/main/java/com/mp3/analukpat/mp3_player/WiedergabeTabsActivity.java
@@ -3,11 +3,11 @@ package com.mp3.analukpat.mp3_player;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AddToPlaylistActivity extends AppCompatActivity {
-    //Sicht für hinzufügen von Liedern in die Playlist
+public class WiedergabeTabsActivity extends AppCompatActivity {
+    //Activity, die den Tabhost für die Wiedergabe und die Wiedergabeliste aufruft
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_to_playlist);
+        setContentView(R.layout.activity_wiedergabe_tabs);
     }
 }
