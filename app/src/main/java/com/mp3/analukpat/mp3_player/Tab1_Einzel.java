780c74f1f87a7d52911f5a8b97c077ffa46c7d98
Nasty <anastasia.sklarow@stud.uni-due.de>
2018-05-30 17:10:48 +0200

    Übung_Tabbed Activity

---
+++ app/src/main/java/com/mp3/analukpat/mp3_player/Tab1_Einzel.java
@@ -0,0 +1,16 @@
+package com.mp3.analukpat.mp3_player;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab1_Einzel extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_tab1_einzel, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/mp3/analukpat/mp3_player/Tab2_Wiedergabeliste.java
@@ -0,0 +1,16 @@
+package com.mp3.analukpat.mp3_player;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab2_Wiedergabeliste extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_tab2wiedergabeliste, container, false);
+        return rootView;
+    }
+}
